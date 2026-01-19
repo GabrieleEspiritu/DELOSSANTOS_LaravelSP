dc6a8d72ce503168c679a2c4b954dc775ce00f4e
Gabriele Espiritu <edelossantos_24000001315@uic.edu.ph>
2026-01-19 12:57:48 +0800

    new chnage

--- public/index.php
+++ public/index.php
@@ -17,12 +17,6 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
+$app->handleRequest(
+    Request::capture()
 );
-
-$response->send();
-
-$kernel->terminate($request, $response);
